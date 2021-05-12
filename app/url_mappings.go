501f7c0883a515389aabe225e327ce4df3e74a97
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-12 17:36:33 +0900

    fmt

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -6,8 +6,8 @@ import (
 )
 
 func mapUrls() {
-	router.GET("/ping",ping.Ping)
-	router.GET("/users/:user_id",users.GetUser)
-	router.GET("/users/search",users.SearchUser)
-	router.POST("/users",users.CreateUser)
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	router.GET("/users/search", users.SearchUser)
+	router.POST("/users", users.CreateUser)
 }
