a92e9bace1b9df1ea08b414d89f010fee7319b3e
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-12 17:36:43 +0900

    create error res

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,6 +3,9 @@ package users
 import (
 	"net/http"
 
+	"github.com/cota-eng/go-users-api/domain/users"
+	"github.com/cota-eng/go-users-api/services"
+	"github.com/cota-eng/go-users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +15,37 @@ var counter int
 func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented,"implement")
 }
-
+ 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement")
+	var user users.User
+	// fmt.Println(user)
+	// bytes,err := ioutil.ReadAll(c.Request.Body)
+	// if err != nil{
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// if err := json.Unmarshal(bytes,&user);err != nil{
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	if err := c.ShouldBindJSON(&user);err!=nil{
+		restErr := errors.NewBandleRequestErrer("invalid json body")
+		c.JSON(restErr.Status,restErr)
+		// TODO: handle json err
+		// log.Fatalln(err)
+		return
+	}
+
+	result,saveErr:=services.CreateUser(user)
+	if saveErr !=nil{
+		c.JSON(saveErr.Status,saveErr)
+		return
+	}
+	// fmt.Println(string(bytes))
+	// fmt.Println(user)
+	// c.String(http.StatusNotImplemented,"implement")
+	c.JSON(http.StatusCreated,result)
 }
 
 func SearchUser(c *gin.Context) {
