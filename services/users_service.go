7595bca63af692b686f09df230170880ac5422f2
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-12 17:36:59 +0900

    error

--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,20 @@
 package services
+
+import (
+	"net/http"
+
+	"github.com/cota-eng/go-users-api/domain/users"
+	"github.com/cota-eng/go-users-api/utils/errors"
+)
+
+
+func CreateUser(user users.User) (*users.User,*errors.RestErr) {
+	// no err and no user
+	return nil,nil
+
+	return &user,nil
+
+	return &user,&errors.RestErr{
+		Status: http.StatusInternalServerError,
+	}
+}
