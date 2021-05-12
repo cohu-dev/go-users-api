8239218a5c231e474bdea47c4c1c873e423f708d
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-12 14:52:32 +0900

    endpoint

---
+++ app/url_mappings.go
@@ -0,0 +1,13 @@
+package app
+
+import (
+	"github.com/cota-eng/go-users-api/controllers/ping"
+	"github.com/cota-eng/go-users-api/controllers/users"
+)
+
+func mapUrls() {
+	router.GET("/ping",ping.Ping)
+	router.GET("/users/:user_id",users.GetUser)
+	router.GET("/users/search",users.SearchUser)
+	router.POST("/users",users.CreateUser)
+}
