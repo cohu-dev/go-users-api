39ad7a1d364dffc622f08da7cd884d4ef94dec19
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-16 23:55:22 +0900

    add method of User

--- services/users_service.go
+++ services/users_service.go
@@ -1,20 +1,23 @@
 package services
 
 import (
-	"net/http"
-
 	"github.com/cota-eng/go-users-api/domain/users"
 	"github.com/cota-eng/go-users-api/utils/errors"
 )
 
 
 func CreateUser(user users.User) (*users.User,*errors.RestErr) {
-	// no err and no user
-	return nil,nil
-
+	if err:=user.Validate();err!=nil{
+		return nil,err
+	}
+	if err:=user.Save();err!=nil{
+		return nil,err
+	}
 	return &user,nil
 
-	return &user,&errors.RestErr{
-		Status: http.StatusInternalServerError,
-	}
+	// return &user,nil
+
+	// return &user,&errors.RestErr{
+	// 	Status: http.StatusInternalServerError,
+	// }
 }
